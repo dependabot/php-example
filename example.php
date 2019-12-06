089175a0eab9223b8cf1dc9dc2bb3e366f8298ce
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2019-12-06 14:39:57 -0300

    :art: Improve structure
    Improve structure of PHP example.
    
    Note:
    - `?>` needs to be ommited. 
    
    References:
    - [Why would one omit the close tag?] (https://stackoverflow.com/questions/4410704/why-would-one-omit-the-close-tag)

--- example.php
+++ example.php
@@ -1,3 +1,8 @@
 <?php
-echo "This is some PHP, I think...";
-?>
+$stringToBeCounted = "This is PHP with Dependabot managing dependencies";
+$stringLength = strlen($stringToBeCounted);
+echo "{$stringToBeCounted} and my length is: {$stringLength}";
+
+// Output:
+// 
+// This is PHP with Dependabot managing dependencies and my length is: 49
